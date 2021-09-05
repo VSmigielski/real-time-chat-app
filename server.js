13892ef136d5bdf60522df84f9192551d22fbbb7
Veronica <veronicasmigielski@gmail.com>
2021-09-05 17:41:18 +0200

    Real-time chat application

---
+++ server.js
@@ -0,0 +1,23 @@
+const server = require('http').createServer();
+const options = { cors:true,
+    origins:["http://127.0.0.1:5500"], };
+const io = require('socket.io')(server, options);
+io.on('connection', socket => { /* ... */ });
+
+const users = {}
+
+io.on('connection', socket => {
+  socket.on('new-user', name => {
+    users[socket.id] = name
+    socket.broadcast.emit('user-connected', name)
+  })
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+  })
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+  })
+})
+
+server.listen(3000);
